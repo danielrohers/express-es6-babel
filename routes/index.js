3fe5d212bc01da23cc8a03fde153cff948f56c54
Daniel Röhers Moura <rohersmoura@gmail.com>
2015-12-14 20:57:38 -0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import express from 'express';
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+    res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import express from 'express';
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', (req, res, next) => {
+    res.send('respond with a resource');
+});
+
+module.exports = router;
